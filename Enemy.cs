e69510a851c006dfe0babecb102806ce9aec20b8
Mason <masonkuhn.1804@gmail.com>
2024-11-03 00:24:29 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-1, -1, 0) * Time.deltaTime * 2f);
+
+        if (transform.position.x < -11f)
+        {
+            Destroy(this.gameObject);
+        }
+        if (transform.position.y < -8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
